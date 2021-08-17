ab9e532641449f739c76065b0a0591f5299ba68e
junzzan9 <ojs2681@gmail.com>
2021-08-17 10:18:45 +0900

    08/17 android mysite

---
+++ app/src/main/java/com/javaex/mysite/MainActivity.java
@@ -0,0 +1,20 @@
+package com.javaex.mysite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //field
+
+    //constructor
+
+    //method g/s
+
+    //method general
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
